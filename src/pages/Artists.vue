b006a9f3018d7dc738d93d7bb757c92a52d28cff
Luca <luca.tagliavini5@studio.unibo.it>
2023-06-26 01:42:02 +0200

    filter artists if they have tracks by default

--- src/pages/Artists.vue
+++ src/pages/Artists.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useInfiniteQuery } from "@tanstack/vue-query";
 
 import Image from '@/components/Image.vue'
@@ -14,25 +15,24 @@ const {
   hasNextPage,
 } = useInfiniteQuery({
   queryKey: ['artists'],
-  queryFn: paginated<ArtistsDocument>(ARTISTS_PATH, { include: ['images'], page: { size: 20 } }),
+  queryFn: paginated<ArtistsDocument>(ARTISTS_PATH, { include: ['images'], filter: { 'tracks': 'true' }, page: { size: 20 } }),
   getNextPageParam: (last: ArtistsDocument, _) => last.data.length > 0 ? last.data[last.data.length - 1].id : null
 })
 await suspense()
+
+const imageForArtist = computed(() => data.value.pages.map(({ data }) => data.map(artist => artist.relationships?.images?.data[0])))
 </script>
 
 <template>
   <main class="h-full overflow-auto">
     <div v-for="page, i in data.pages">
       Page {{ i }}
-      <template v-for="artist in page.data" :key="artist.id">
-        <template v-if="artist.relationships?.releases?.data?.length > 0">
-          <div>
-            {{ artist.attributes.name }}
-            {{ artist.attributes.description || "No description" }}
-            <Image :id="img.id" v-for="img in artist.relationships?.images?.data" :key="img.id" />
-          </div>
-          <div class="divider" />
-        </template>
+      <template v-for="artist, j in page.data" :key="artist.id">
+        <div>
+          {{ artist.attributes.name }}
+          <Image :id="imageForArtist[i][j].id" v-if="imageForArtist[i][j]" :key="imageForArtist[i][j].id" />
+        </div>
+        <div class="divider" />
       </template>
       <button @click="fetchNextPage" v-if="hasNextPage">next</button>
     </div>
